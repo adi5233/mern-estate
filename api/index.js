74ea60ba3307bec9f04517c12ca3d292fa1ef41c
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2023-12-01 19:19:28 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// can not use env variable directly to backend, need dotenv packge
+dotenv.config();
+
+// connect to mongodb database
+mongoose
+  .connect(process.env.MONGO)
+  .then((data) => {
+    console.log(` - Mongodb connected with server.`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// creating express server
 const app = express();
-
 app.listen(3000, () => {
-  console.log("Server is running on port 3000....!");
+  console.log(" - Server is running on port 3000.");
 });
-
-// app.get('/', )
