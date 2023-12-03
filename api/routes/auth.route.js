7e1f73d28e4dec7c9478cbf16455643faf51cb7b
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2023-12-03 11:44:26 +0530

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,14 +2,14 @@ import express from "express";
 import {
   //   google,
   //   signOut,
-  //   signin,
+  signin,
   signup,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-// router.post("/signin", signin);
+router.post("/signin", signin);
 // router.post('/google', google);
 // router.get('/signout', signOut)
 
