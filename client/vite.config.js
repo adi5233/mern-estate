89c16d9f3d426b9f4c239edaff715615f3d63617
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2023-12-03 11:16:52 +0530

    complete signup page funtionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // if we create any req from frontend localhost:5731 it will go to localhost:3000
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
-})
+});
