4d7b7ef3052ead41dc39aae5b46d915f91cc6ece
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2023-12-04 12:53:37 +0530

    add image slider

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -51,7 +51,6 @@ export const deleteUser = async (req, res, next) => {
 };
 
 export const getUserListings = async (req, res, next) => {
-  console.log("calling get use listings");
   if (req.user.id === req.params.id) {
     try {
       const listings = await Listing.find({ userRef: req.params.id });
