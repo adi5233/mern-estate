6c1d2a4709a4006092980ccb3958a4b5203bebde
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2023-12-04 12:08:47 +0530

    creating getuserlisting API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,8 +3,8 @@ import {
   test,
   deleteUser,
   updateUser,
-  //   getUserListings,
-  //   getUser,
+  getUserListings,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -16,7 +16,7 @@ router.get("/test1", (req, res) => {
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-// router.get("/listings/:id", verifyToken, getUserListings);
-// router.get("/:id", verifyToken, getUser);
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
